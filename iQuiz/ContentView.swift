8da3b32f79e10e23dd713d23c92c6ae9d31e5c25
Shiina Sugioka <shiina12@uw.edu>
2024-05-07 23:32:20 -0700

    adding starter code

---
+++ iQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iQuiz
+//
+//  Created by stlp on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by stlp on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
