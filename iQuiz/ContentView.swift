71cabc0b1922258d8745a5e81d454e06ad1eb6a2
Shiina Sugioka <shiina12@uw.edu>
2024-05-09 18:44:17 -0700

    added scaffolding for table view

--- iQuiz/ContentView.swift
+++ iQuiz/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let categories: [Category] = [
+        Category(name: "Mathematics", description: "Take a simple math quiz.", icon: "x.squareroot"),
+        Category(name: "Marvel Super Heroes", description: "How much do you know about heroes?", icon: "figure.archery"),
+        Category(name: "Science", description: "Test your science knowledge.", icon: "atom")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            
+            List(categories, id: \.self) { category in
+                NavigationLink(destination: Text("\(category.description)")) {
+                    HStack {
+                        Image(systemName: category.icon)
+                            .padding(.trailing, 10)
+                            .font(.system(size: 30))
+                            .foregroundStyle(Color(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/))
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(category.name)
+                                .font(.system(size: 20))
+                                
+                            Text(category.description)
+                                .foregroundStyle(Color(.gray))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("iQuiz")
         }
-        .padding()
     }
+}
+
+struct Category: Hashable {
+    let name: String
+    let description: String
+    let icon: String
 }
 
 #Preview {
